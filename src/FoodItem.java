4a340ab39d7c5e3d0dd1a4112a7e9b40211ce7c3
LukasEliasson <lukas.eo.eliasson@gmail.com>
2026-03-20 11:15:37 +0100

    Created basic food items

--- src/FoodItem.java
+++ src/FoodItem.java
@@ -1,2 +1,14 @@
 public class FoodItem {
+
+    private String name;
+    private int id;
+
+    public String getName() {
+        return this.name;
+    };
+
+    public int getId() {
+        return this.id;
+    }
+
 }
---
+++ src/Fries.java
@@ -0,0 +1,11 @@
+public class Fries extends FoodItem {
+
+    private String name;
+    private int id;
+
+    public Fries() {
+        this.name = "Fries";
+        this.id = 2;
+    }
+
+}
