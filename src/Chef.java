bf6611430e40005adb0f0fe37e274da8bf399f89
07antwin <07antwin@gafe.molndal.se>
2026-03-27 09:30:22 +0100

    hej

--- src/Chef.java
+++ src/Chef.java
@@ -1,2 +1,21 @@
 public class Chef extends Agent {
+    public boolean isAvailable = true;
+    private Order currentOrder = null;
+
+    public void update() {
+        if (isAvailable) {
+            OrderSystem system = OrderSystem.getOrderSystem();
+            Order nextOrder = system.takeNextOrder();
+
+            if (nextOrder != null) {
+                this.currentOrder = nextOrder;
+                this.isAvailable = false; }
+        } else {
+            // Logic to cook food
+        }
+    }
+
+    public Order getCurrentOrder() {
+        return currentOrder;
+    }
 }
--- src/FoodItem.java
+++ src/FoodItem.java
@@ -1,2 +1,20 @@
 public class FoodItem {
+    public enum CourseType {
+        BURGER,
+        DRINK,
+        FRIES,
+    }
+
+    private String name;
+    private CourseType type;
+    private boolean isCooked = false;
+
+    public FoodItem(String name, CourseType type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public CourseType getType() { return type; }
+    public void setCooked(boolean cooked) { this.isCooked = cooked; }
+    public boolean isCooked() { return isCooked; }
 }
