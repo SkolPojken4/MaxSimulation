15cfc48c598d8a96f646d455e315ddcb5cda3f4e
07antwin <07antwin@gafe.molndal.se>
2026-03-27 10:50:46 +0100

    hej

--- src/Chef.java
+++ src/Chef.java
@@ -1,6 +1,10 @@
 public class Chef extends Agent {
     public boolean isAvailable = true;
     private Order currentOrder = null;
+    private int numInOrder = null
+    private FoodItem currentItem = null;
+    private int cookingTimer = 0;
+    private final int cookingTime= 100; //This should be in the food
 
     public void update() {
         if (isAvailable) {
@@ -9,8 +13,18 @@ public class Chef extends Agent {
 
             if (nextOrder != null) {
                 this.currentOrder = nextOrder;
-                this.isAvailable = false; }
+                this.isAvailable = false;
+                this.cookingTimer = cookingTime;}
         } else {
+            if (cookingTimer != 0) {
+                cookingTimer =- 1;
+            }
+            if (cookingTimer == 0) {
+                case currentOrder[numInOrder] = FoodItem.CourseType.FRIES;
+                try { num
+
+                }
+            }
             // Logic to cook food
         }
     }
